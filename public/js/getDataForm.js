8f2c3bf9805f1597219825d164c596b3e11a9a71
Alfredcss <alfredofe.ti22@utsjr.edu.mx>
2024-07-25 09:49:42 -0600

    Terminado segun

--- public/js/getDataForm.js
+++ public/js/getDataForm.js
@@ -1,118 +1,95 @@
-import { getFirestore, collection, addDoc } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+import { getFirestore, collection, addDoc, Timestamp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+import { getStorage, ref, uploadBytes, getDownloadURL } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-storage.js";
 
 const db = getFirestore();
+const storage = getStorage();
 
-const createExpediente = (name, apellidos, edad, fechaNacimiento, genero, direccion, telefono, grupoEtnico, antecedentesHeredoFamiliares, antecedentesPersonalesPatologicos, padecimientoActual, interrogatorioAparatos, habitusExterior, signosVitales, peso, talla, datosCabeza, datosCuello, datosTorax, datosAbdomen, datosMiembros, datosGenitales, resultadosEstudios, diagnosticos, pronostico, indicacionTerapeutica, evolucion, signosVitalesEvolucion, resultadosRelevantes, diagnosticosEvolucion, pronosticoEvolucion, tratamientoIndicaciones, criteriosDiagnosticos, planEstudios, sugerenciasDiagnosticas, establecimientoEnvia, establecimientoReceptor, motivoEnvio, impresionDiagnostica, terapeuticaEmpleada, fechaHoraAtencion, motivoAtencion, resumenInterrogatorio, resultadosRelevantesUrgencias, diagnosticosUrgencias, tratamientoPronostico) => {
+const createExpediente = (expedienteData, pdfUrl) => {
     return addDoc(collection(db, 'expediente'), {
-        name,
-        apellidos,
-        edad,
-        fechaNacimiento,
-        genero,
-        direccion,
-        telefono,
-        grupoEtnico,
-        antecedentesHeredoFamiliares,
-        antecedentesPersonalesPatologicos,
-        padecimientoActual,
-        interrogatorioAparatos,
-        habitusExterior,
-        signosVitales,
-        peso,
-        talla,
-        datosCabeza,
-        datosCuello,
-        datosTorax,
-        datosAbdomen,
-        datosMiembros,
-        datosGenitales,
-        resultadosEstudios,
-        diagnosticos,
-        pronostico,
-        indicacionTerapeutica,
-        evolucion,
-        signosVitalesEvolucion,
-        resultadosRelevantes,
-        diagnosticosEvolucion,
-        pronosticoEvolucion,
-        tratamientoIndicaciones,
-        criteriosDiagnosticos,
-        planEstudios,
-        sugerenciasDiagnosticas,
-        establecimientoEnvia,
-        establecimientoReceptor,
-        motivoEnvio,
-        impresionDiagnostica,
-        terapeuticaEmpleada,
-        fechaHoraAtencion,
-        motivoAtencion,
-        resumenInterrogatorio,
-        resultadosRelevantesUrgencias,
-        diagnosticosUrgencias,
-        tratamientoPronostico
+        ...expedienteData,
+        pdfUrl
     });
 }
 
 document.addEventListener('DOMContentLoaded', () => {
     const expedienteForm = document.getElementById('expedienteform');
 
+    // Set the current date and time in the hidden field
+    
+
     expedienteForm.addEventListener('submit', async (e) => {
         e.preventDefault();
-        
-        const name = expedienteForm['nombre'].value;
-        const apellidos = expedienteForm['apellidos'].value;
-        const edad = expedienteForm['edad'].value;
-        const fechaNacimiento = expedienteForm['fechaNacimiento'].value;
-        const genero = expedienteForm['genero'].value;
-        const direccion = expedienteForm['direccion'].value;
-        const telefono = expedienteForm['telefono'].value;
-        const grupoEtnico = expedienteForm['grupoetnico'].value;
-        const antecedentesHeredoFamiliares = expedienteForm['antecedentesheredofamiliares'].value;
-        const antecedentesPersonalesPatologicos = expedienteForm['antecedentespersonales'].value;
-        const padecimientoActual = expedienteForm['padecimientoactual'].value;
-        const interrogatorioAparatos = expedienteForm['interrogatorioaparatos'].value;
-        const habitusExterior = expedienteForm['habitus'].value;
-        const signosVitales = expedienteForm['signosvitales'].value;
-        const peso = expedienteForm['peso'].value;
-        const talla = expedienteForm['talla'].value;
-        const datosCabeza = expedienteForm['datoscabeza'].value;
-        const datosCuello = expedienteForm['datoscuello'].value;
-        const datosTorax = expedienteForm['datostorax'].value;
-        const datosAbdomen = expedienteForm['datosabdomen'].value;
-        const datosMiembros = expedienteForm['datosmiembros'].value;
-        const datosGenitales = expedienteForm['datosgenitales'].value;
-        const resultadosEstudios = expedienteForm['resultadosestudios'].value;
-        const diagnosticos = expedienteForm['diagnosticos'].value;
-        const pronostico = expedienteForm['pronostico'].value;
-        const indicacionTerapeutica = expedienteForm['indicacionterapeutica'].value;
-        const evolucion = expedienteForm['evolucion'].value;
-        const signosVitalesEvolucion = expedienteForm['signosvitalesevolucion'].value;
-        const resultadosRelevantes = expedienteForm['resultadosrelevantes'].value;
-        const diagnosticosEvolucion = expedienteForm['diagnosticosevolucion'].value;
-        const pronosticoEvolucion = expedienteForm['pronosticoevolucion'].value;
-        const tratamientoIndicaciones = expedienteForm['tratamientoindicaciones'].value;
-        const criteriosDiagnosticos = expedienteForm['criteriosdiagnosticos'].value;
-        const planEstudios = expedienteForm['planestudios'].value;
-        const sugerenciasDiagnosticas = expedienteForm['sugerenciasdiagnosticas'].value;
-        const establecimientoEnvia = expedienteForm['establecimientoenvia'].value;
-        const establecimientoReceptor = expedienteForm['establecimientoreceptor'].value;
-        const motivoEnvio = expedienteForm['motivoenvio'].value;
-        const impresionDiagnostica = expedienteForm['impresiondiagnostica'].value;
-        const terapeuticaEmpleada = expedienteForm['terapeuticaempleada'].value;
-        const fechaHoraAtencion = expedienteForm['fechahoraatencion'].value;
-        const motivoAtencion = expedienteForm['motivoatencion'].value;
-        const resumenInterrogatorio = expedienteForm['resumeninterrogatorio'].value;
-        const resultadosRelevantesUrgencias = expedienteForm['resultadosrelevantesurgencias'].value;
-        const diagnosticosUrgencias = expedienteForm['diagnosticosurgencias'].value;
-        const tratamientoPronostico = expedienteForm['tratamientopronostico'].value;
-        
-        try {
-            await createExpediente(name, apellidos, edad, fechaNacimiento, genero, direccion, telefono, grupoEtnico, antecedentesHeredoFamiliares, antecedentesPersonalesPatologicos, padecimientoActual, interrogatorioAparatos, habitusExterior, signosVitales  , peso, talla, datosCabeza, datosCuello, datosTorax, datosAbdomen, datosMiembros, datosGenitales, resultadosEstudios, diagnosticos, pronostico, indicacionTerapeutica, evolucion, signosVitalesEvolucion, resultadosRelevantes, diagnosticosEvolucion, pronosticoEvolucion, tratamientoIndicaciones, criteriosDiagnosticos, planEstudios, sugerenciasDiagnosticas, establecimientoEnvia, establecimientoReceptor, motivoEnvio, impresionDiagnostica, terapeuticaEmpleada, fechaHoraAtencion, motivoAtencion, resumenInterrogatorio, resultadosRelevantesUrgencias, diagnosticosUrgencias, tratamientoPronostico);
-            alert('Expediente guardado con éxito');
-            expedienteForm.reset();
-        } catch (error) {
-            console.error('Error guardando el expediente: ', error);
-            alert('Hubo un error guardando el expediente');
+        const expedienteData = {
+            name: expedienteForm['nombre'].value,
+            apellidos: expedienteForm['apellidos'].value,
+            edad: expedienteForm['edad'].value,
+            fechaNacimiento: expedienteForm['fechaNacimiento'].value,
+            genero: expedienteForm['genero'].value,
+            direccion: expedienteForm['direccion'].value,
+            telefono: expedienteForm['telefono'].value,
+            grupoEtnico: expedienteForm['grupoetnico'].value,
+            fechaRegistro: expedienteForm['fechaRegistro'].value,
+            antecedentesHeredoFamiliares: expedienteForm['antecedentesheredofamiliares'].value,
+            antecedentesPersonalesPatologicos: expedienteForm['antecedentespersonales'].value,
+            padecimientoActual: expedienteForm['padecimientoactual'].value,
+            interrogatorioAparatos: expedienteForm['interrogatorioaparatos'].value,
+            habitusExterior: expedienteForm['habitus'].value,
+            peso: expedienteForm['peso'].value,
+            talla: expedienteForm['talla'].value,
+            fc: expedienteForm['FC'].value,
+            ta: expedienteForm['TA'].value,
+            fr: expedienteForm['FR'].value,
+            t: expedienteForm['T'].value,
+            datosCabeza: expedienteForm['datoscabeza'].value,
+            datosCuello: expedienteForm['datoscuello'].value,
+            datosTorax: expedienteForm['datostorax'].value,
+            datosAbdomen: expedienteForm['datosabdomen'].value,
+            datosMiembros: expedienteForm['datosmiembros'].value,
+            datosGenitales: expedienteForm['datosgenitales'].value,
+            resultadosEstudios: expedienteForm['resultadosestudios'].value,
+            diagnosticos: expedienteForm['diagnosticos'].value,
+            pronostico: expedienteForm['pronostico'].value,
+            indicacionTerapeutica: expedienteForm['indicacionterapeutica'].value,
+            evolucion: expedienteForm['evolucion'].value,
+            signosVitalesEvolucion: expedienteForm['signosvitalesevolucion'].value,
+            resultadosRelevantes: expedienteForm['resultadosrelevantes'].value,
+            diagnosticosEvolucion: expedienteForm['diagnosticosevolucion'].value,
+            pronosticoEvolucion: expedienteForm['pronosticoevolucion'].value,
+            tratamientoIndicaciones: expedienteForm['tratamientoindicaciones'].value,
+            criteriosDiagnosticos: expedienteForm['criteriosdiagnosticos'].value,
+            planEstudios: expedienteForm['planestudios'].value,
+            sugerenciasDiagnosticas: expedienteForm['sugerenciasdiagnosticas'].value,
+            motivoAtencion: expedienteForm['motivoatencion'].value,
+            resumenInterrogatorio: expedienteForm['resumeninterrogatorio'].value,
+            resultadosRelevantesUrgencias: expedienteForm['resultadosrelevantesurgencias'].value,
+            diagnosticosUrgencias: expedienteForm['diagnosticosurgencias'].value,
+            tratamientoPronostico: expedienteForm['tratamientopronostico'].value,
+            fechaCreacion: expedienteForm['creationDate'].value // Add creation date
+        };
+
+        const pdfFile = expedienteForm['pdfFile'].files[0];
+
+        if (pdfFile) {
+            const pdfRef = ref(storage, 'pdfs/' + pdfFile.name);
+            try {
+                const snapshot = await uploadBytes(pdfRef, pdfFile);
+                const pdfUrl = await getDownloadURL(snapshot.ref);
+                await createExpediente(expedienteData, pdfUrl);
+                alert('Expediente guardado con éxito');
+                expedienteForm.reset();
+            } catch (error) {
+                console.error('Error guardando el expediente: ', error);
+                alert('Hubo un error guardando el expediente');
+            }
+        } else {
+            try {
+                await createExpediente(expedienteData, null);
+                alert('Expediente guardado con éxito sin archivo PDF');
+                expedienteForm.reset();
+            } catch (error) {
+                console.error('Error guardando el expediente: ', error);
+                alert('Hubo un error guardando el expediente');
+            }
         }
     });
 });
